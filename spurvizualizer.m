ea63ce56d5d60b6fdcf0bb9bf8bb5e19b7dc530d
Tim Poole <102406661+Timothy-Poole@users.noreply.github.com>
2023-10-30 14:20:28 +1000

    Update spurvizualizer.m

--- spurvizualizer.m
+++ spurvizualizer.m
@@ -1,13 +1,13 @@
-clc;
-close all;
-Mixer = rfckt.mixer('FLO', 2e9);      % Flo = 2GHz local oscillator.
+clc;                                        % clear command line
+close all;                                  % close all figures
+Mixer = rfckt.mixer('FLO', 2e9);            % FLO = Frequency of Local Oscillator
 read(Mixer,'samplespur1.s2d');
-disp(Mixer)
+disp(Mixer)                                 % displays info about the mixer in the command window below
 IMT = Mixer.MixerSpurData.data;
-CktIndex = 1;       % Plot the output only
-Pin = 0;          % Input power is -0dBm
-Fin = 2.5e9;        % Input frequency is 2.5GHz
+CktIndex = 1;                               % 1 = Plot the output only
+Pin = 0;                                    % Input power is -0dBm
+Fin = 2.5e9;                                % Input frequency
 figure
 plot(Mixer,'MIXERSPUR',CktIndex,Pin,Fin);
-yline(-25,'-','Spurious');
-yline(-55,'-','Image');
+yline(-25,'-','Spurious');                  %this creates the line on the graph
+yline(-55,'-','Image');                     %this creates the line on the graph
