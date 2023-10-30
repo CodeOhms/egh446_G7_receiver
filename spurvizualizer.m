9e5757fb9a3fcc918b217160db82e1f189db9c07
Tim Poole <102406661+Timothy-Poole@users.noreply.github.com>
2023-10-30 14:26:42 +1000

    Update spurvizualizer.m

--- spurvizualizer.m
+++ spurvizualizer.m
@@ -1,11 +1,15 @@
+%% this section is for editing the mixer's data file
+edit('samplespur1.s2d');                    % this data file contains paramaters that adjust the mixer
+%% code to run
 clc;                                        % clear command line
 close all;                                  % close all figures
+clear;                                      % close all data files and objects
 Mixer = rfckt.mixer('FLO', 2e9);            % FLO = Frequency of Local Oscillator
 read(Mixer,'samplespur1.s2d');
 disp(Mixer)                                 % displays info about the mixer in the command window below
 IMT = Mixer.MixerSpurData.data;
 CktIndex = 1;                               % 1 = Plot the output only
-Pin = 0;                                    % Input power is -0dBm
+Pin = -10;                                  % Input power
 Fin = 2.5e9;                                % Input frequency
 figure
 plot(Mixer,'MIXERSPUR',CktIndex,Pin,Fin);
